7aeab2fcabdc8ea0a33bd65b4b00990048e1c223
Rin <kia01182008@gmail.com>
2026-01-31 11:49:20 +0100

    upd

--- app.js
+++ app.js
@@ -2,22 +2,18 @@ const slider = document.querySelector('.slider__input');
 const image = document.querySelector('.slider__image');
 const box = document.getElementById('box');
 
-const handleSliderChange = _.debounce((event) => {
-  const value = event.target.value;
-  const newSize = 100 + value * 3;
-  
-  image.style.width = newSize + 'px';
-  image.style.height = newSize + 'px';
-  
-  console.log(`Slider value: ${value}, Image size: ${newSize}px`);
-}, 300);
+function handleSliderChange(event) {
+    const value = event.target.value;
+    console.log(value);
+    image.style.width = value + 'px';
+}
 
-slider.addEventListener('input', handleSliderChange);
+function mousePos(event) {
+    console.log('Mouse X:', event.clientX, 'Mouse Y:', event.clientY);
+    box.style.left = event.clientX + 'px';
+    box.style.top = event.clientY + 'px';
+    box.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
-const handleMouseMove = _.debounce((event) => {
-  box.style.left = event.clientX + 'px';
-  box.style.top = event.clientY + 'px';
-}, 100);
-
-document.addEventListener('mousemove', handleMouseMove);
-
+slider.addEventListener('input', _.debounce(handleSliderChange, 500));
+document.addEventListener('mousemove', _.debounce(mousePos, 100));
